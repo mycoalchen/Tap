4d1a78c34e537bed61b563435af9103ee4ccb6ba
mycoalchen <chen.michael.1116@gmail.com>
2023-04-11 23:14:22 -0400

    Initial Commit

---
+++ Hoodsend/HoodsendApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoodsendApp.swift
+//  Hoodsend
+//
+//  Created by Michael Chen on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HoodsendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
