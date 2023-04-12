29c783742ae8a67bdcf2e099cb48121d1a18c117
mycoalchen <chen.michael.1116@gmail.com>
2023-04-11 23:38:38 -0400

    first commit - homepage

--- Hoodsend/ContentView.swift
+++ Hoodsend/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10)
+            {
+            Text("Welcome to Hoodsend!")
+                .font(.system(size: 40))
+                .multilineTextAlignment(.center)
+                .padding([.bottom], 40)
+            Text("I'm a...")
+                .font(.system(size: 30))
+                .padding([.bottom], 20)
+            HStack {
+                Button(action: {print("Merchant")}) {
+                    Text("Merchant")
+                        .foregroundColor(Color(red: 1, green: 1, blue: 1))
+                        .font(.system(size: 25))
+                }
+                .padding()
+                .background(Color(red: 0.5, green: 0.0, blue: 0.5))
+                .clipShape(Capsule())
+                Button(action: {print("Cusomter")}) {
+                    Text("Customer")
+                        .font(.system(size: 25))
+                        .foregroundColor(Color(red: 1, green: 1, blue: 1))
+                }
+                .padding()
+                .background(Color(red: 0, green: 0.5, blue: 0))
+                .clipShape(Capsule())
+            }
         }
         .padding()
     }
