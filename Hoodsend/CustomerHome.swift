d5c62290353ec841929a2db2db25a8db75762c10
mycoalchen <chen.michael.1116@gmail.com>
2023-04-15 00:32:14 -0400

    fix nested navigation

--- Hoodsend/CustomerHome.swift
+++ Hoodsend/CustomerHome.swift
@@ -26,84 +26,84 @@ struct CustomerHome: View {
     
     
     var body: some View {
-        NavigationStack {
-            VStack(spacing: 0) {
-                HStack(spacing: 0) {
-                    Text("Shops")
-                        .font(.system(size: 24))
+        VStack(spacing: 0) {
+            HStack(spacing: 0) {
+                Text("Shops")
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 36)
+                    .padding(.top, 12)
+                    .padding(.bottom, 12)
+                ZStack {
+                    Circle()
+                        .stroke(tapPurple, lineWidth: 0)
+                        .background(Circle().foregroundColor(tapPurple))
+                    Text("ME")
+                        .font(.system(size: 16))
                         .fontWeight(.bold)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 36)
-                        .padding(.top, 12)
-                        .padding(.bottom, 12)
-                    ZStack {
-                        Circle()
-                            .stroke(tapPurple, lineWidth: 0)
-                            .background(Circle().foregroundColor(tapPurple))
-                        Text("ME")
-                            .font(.system(size: 16))
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .frame(width: 25, height: 25, alignment: .center)
-                            .padding(.all, 8)
-                    }
-                    .padding(.trailing, 60)
-                    .frame(width: 35, height: 35)
+                        .foregroundColor(.white)
+                        .frame(width: 25, height: 25, alignment: .center)
+                        .padding(.all, 8)
                 }
-                Rectangle()
-                    .fill(.black)
-                    .frame(width: UIScreen.screenWidth - 48, height: 0.75)
-                Spacer(minLength: 12)
-                ScrollView {
-                    ForEach(merchants) { merchant in
-                        Button() {
-                            selectedMerchant = merchant
-                            isMerchantSelected = true
-                        } label: {
-                            HStack() {
-                                VStack(alignment: .leading, spacing: 0) {
-                                    Text(merchant.name).foregroundColor(.black).fontWeight(.bold)
-                                    Text(merchant.location).foregroundColor(.black)
-                                }
-                                Spacer()
-                                Text("$\(merchant.balance, specifier: "%.2f")")
-                                    .foregroundColor(.black)
+                .padding(.trailing, 60)
+                .frame(width: 35, height: 35)
+            }
+            Rectangle()
+                .fill(.black)
+                .frame(width: UIScreen.screenWidth - 48, height: 0.75)
+            Spacer(minLength: 12)
+            ScrollView {
+                ForEach(merchants) { merchant in
+                    Button() {
+                        selectedMerchant = merchant
+                        isMerchantSelected = true
+                    } label: {
+                        HStack() {
+                            VStack(alignment: .leading, spacing: 0) {
+                                Text(merchant.name).foregroundColor(.black).fontWeight(.bold)
+                                Text(merchant.location).foregroundColor(.black)
                             }
-                            .padding(.horizontal, 16)
+                            Spacer()
+                            Text("$\(merchant.balance, specifier: "%.2f")")
+                                .foregroundColor(.black)
                         }
-                        .padding(.vertical, 12)
-                        .frame(maxWidth: .infinity)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(Color.gray, lineWidth: 0.5)
-                        )
+                        .padding(.horizontal, 16)
                     }
                     .padding(.vertical, 12)
-                    .padding(.horizontal, 12)
+                    .frame(maxWidth: .infinity)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray, lineWidth: 0.5)
+                    )
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.horizontal, 16)
-                Spacer(minLength: 24)
-                Button() {} label: {
-                    Text("Add Merchant")
-                        .font(.system(size: 25))
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 1, green: 1, blue: 1))
-                }
-                .padding()
-                .background(tapPurple)
-                .clipShape(Capsule())
-                Spacer()
+                .padding(.vertical, 12)
+                .padding(.horizontal, 12)
             }
-            .navigationDestination(isPresented: $isMerchantSelected) {
-                PlaceOrder(shopName: selectedMerchant.name, balance: selectedMerchant.balance)
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal, 16)
+            Spacer(minLength: 24)
+            Button() {} label: {
+                Text("Add Merchant")
+                    .font(.system(size: 25))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 1, green: 1, blue: 1))
             }
+            .padding()
+            .background(tapPurple)
+            .clipShape(Capsule())
+            Spacer()
         }
+        .navigationDestination(isPresented: $isMerchantSelected) {
+            PlaceOrder(shopName: selectedMerchant.name, balance: selectedMerchant.balance)
+        }   
     }
 }
 
 struct CustomerHome_Previews: PreviewProvider {
     static var previews: some View {
-        CustomerHome()
+        NavigationStack {
+            CustomerHome()
+        }
     }
 }
